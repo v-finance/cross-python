016db727c633c47da7d87366a2a7329b8cfa1052
Erik Janssens <erik.janssens@vortex-financials.be>
2020-07-21 19:19:51 +0200

    add some inet protocol enumerations

--- Modules/_socket/ip_protocols.c
+++ Modules/_socket/ip_protocols.c
@@ -4,31 +4,40 @@
 
 // WinSock2 only protocols
 
+/*
+ * See values in
+ * /usr/i686-w64-mingw32/sys-root/mingw/include/winsock2.h:86
+ * https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml
+ *
+ */
+
 enum {
+
+    // Values ??
+    IPPROTO_EGP = 8,
+    IPPROTO_PIM = 103,
     
-    IPPROTO_ICMP = 5,
-    IPPROTO_IGMP = 5,
-    IPPROTO_GGP = 5,
-    IPPROTO_TCP = 5,
-    IPPROTO_PUP = 5,
-    IPPROTO_UDP = 5,
-    IPPROTO_IDP = 5,
-    IPPROTO_ND = 5,
-    IPPROTO_RAW = 5,
-    IPPROTO_MAX = 5,
-    IPPROTO_HOPOPTS = 5,
-    IPPROTO_IPV4 = 5,
-    IPPROTO_IPV6 = 5,
-    IPPROTO_ROUTING = 5,
-    IPPROTO_FRAGMENT = 5,
-    IPPROTO_ESP = 5,
-    IPPROTO_AH = 5,
-    IPPROTO_ICMPV6 = 5,
-    IPPROTO_NONE = 5,
-    IPPROTO_DSTOPTS = 5,
-    IPPROTO_EGP = 5,
-    IPPROTO_PIM = 5,
-    IPPROTO_ICLFXBM = 5,
+    IPPROTO_ICMP = 1,
+    IPPROTO_IGMP = 2,
+    IPPROTO_GGP = 3,
+    IPPROTO_TCP = 6,
+    IPPROTO_PUP = 12,
+    IPPROTO_UDP = 17,
+    IPPROTO_IDP = 22,
+    IPPROTO_ND = 77,
+    IPPROTO_RAW = 255,
+    IPPROTO_MAX = 256,
+    IPPROTO_HOPOPTS = 0,
+    IPPROTO_IPV4 = 4,
+    IPPROTO_IPV6 = 41,
+    IPPROTO_ROUTING = 43,
+    IPPROTO_FRAGMENT = 44,
+    IPPROTO_ESP = 50,
+    IPPROTO_AH = 51,
+    IPPROTO_ICMPV6 = 58,
+    IPPROTO_NONE = 59,
+    IPPROTO_DSTOPTS = 60,
+    IPPROTO_ICLFXBM = 78,
 
     IPPROTO_ST = 5,
     IPPROTO_CBT = 7,
