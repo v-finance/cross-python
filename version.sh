2332d8f1825fc89080415a2eab5b40f571d4f8a1
Erik Janssens <erik.janssens@vortex-financials.be>
2025-04-05 13:48:23 +0200

    adapt regexp to match newer python versions

--- version.sh
+++ version.sh
@@ -3,4 +3,4 @@
 # to get the python version from configure.ac, first select
 # the version string with sed and remove newlines with tr.
 #
-sed -n "s/^.*PYTHON_VERSION,\s*\(\S*\))$/\1/p" $1 | tr -d "\n"
+sed -n "s/^m4_define.*PYTHON_VERSION.*\([0-9]\+\.[0-9]\+\).*)$/\1/p" $1 | tr -d "\n"
